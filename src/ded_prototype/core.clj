7de22f29ccfda81d9cd297ffb5749545cc770fe8
Matthew Lemon <matt@matthewlemon.com>
2022-11-29 17:17:10 +0000

    playing around with defrecord and other stuff

--- src/ded_prototype/core.clj
+++ src/ded_prototype/core.clj
@@ -1,7 +1,29 @@
 (ns ded-prototype.core
   (:gen-class)
   (:require [clojure.data.csv :as csv]
-            [clojure.java.io :as io]))
+            [clojure.java.io :as io]
+            [clojure.spec.alpha :as s]))
+
+(s/valid? string? "tots") ;; => true
+
+(def n-gt-10 (s/and number? #(> % 10)))
+(s/valid? n-gt-10 11) ;; => true
+
+(defrecord Site [name location rp])
+(defrecord Person [fname lname])
+
+(def baes
+  (->Site "BAES Barrow" "Barrow-in-Furness" (->Person "Todd" "Larken")))
+
+(def coll-string? (s/coll-of string?))
+
+;; data modelling using maps
+(def organisations
+  [
+   {:name "Op 1" :type :operation}
+   {:name "Op 2" :type :site}
+   {:name "Op 3" :type :site}
+   ])
 
 (defn parse-csv [fn]
   (let [data (with-open [reader (io/reader fn)]
@@ -17,6 +39,7 @@
 
 (parse-csv "dscs.csv")
 
+(filter #(= (:type %) :operation) organisations)
 
 
 
