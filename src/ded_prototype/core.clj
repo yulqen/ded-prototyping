532bd28668970bb0aaff264f39a57af3668e95db
Matthew Lemon <matt@matthewlemon.com>
2022-11-29 17:29:24 +0000

    nice comments on the playabout code I've done today

--- src/ded_prototype/core.clj
+++ src/ded_prototype/core.clj
@@ -4,20 +4,36 @@
             [clojure.java.io :as io]
             [clojure.spec.alpha :as s]))
 
+;; quick look at clojure.spec
 (s/valid? string? "tots") ;; => true
 
+;; checking for two characteristics
 (def n-gt-10 (s/and number? #(> % 10)))
 (s/valid? n-gt-10 11) ;; => true
 
+;; creating records
 (defrecord Site [name location rp])
 (defrecord Person [fname lname])
 
+;; func to create site, based on the records
+(defn make-site [name location ^Person rp]
+  (->Site name location rp))
+
+;; create a site
+(def rrdl (make-site "Rosyth" "Rosyth Royal Dockyard" (->Person "Harold" "Clype")))
+
+;; create another site
 (def baes
   (->Site "BAES Barrow" "Barrow-in-Furness" (->Person "Todd" "Larken")))
 
-(def coll-string? (s/coll-of string?))
+;; get the RP for each site
+(defn get-rps [& sites]
+  (map :rp sites))
 
-;; data modelling using maps
+;; call that function
+(get-rps rrdl baes)
+
+;; data modelling using maps - superceded by defrecord above
 (def organisations
   [
    {:name "Op 1" :type :operation}
@@ -25,6 +41,10 @@
    {:name "Op 3" :type :site}
    ])
 
+;; basic filter on a map
+(filter #(= (:type %) :operation) organisations)
+
+;; import dscs from a csv file
 (defn parse-csv [fn]
   (let [data (with-open [reader (io/reader fn)]
                (doall
@@ -37,11 +57,8 @@
                           (keyword (first headers))]
                          [(first f) (read-string (last f))])))))
 
+;; check that the func works
 (parse-csv "dscs.csv")
-
-(filter #(= (:type %) :operation) organisations)
-
-
 
 
 
